e8a354766b1e5372238f1c4ae5c2100983eeb3bd
Bhagavathula Siva Dhatri <bhagavathulasivadhatri@Bhagavathulas-MacBook-Air.local>
2025-03-23 08:37:49 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'medibot',
+    version= '0.1',
+    author = 'Dhatri Bhagavathula',
+    author_email = 'dhatribhagavathula@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
